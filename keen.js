1e3702e9ba37604059a280e14230568588be9551
Malgorzata Felis <malgosia@Malgorzatas-MacBook-Pro-2.local>
2020-06-23 11:37:30 +0200

    keen

--- keen.js
+++ keen.js
@@ -4,22 +4,22 @@ const client = new KeenTracking({
   writeKey: 'D2FC97F6029851794638ED0A17BF1BFCDCA3781ED6DBF5628D403EA1FE5B28FF3D6A178D20FB728A8D62CACF877BE90E3F04976FB633B487270A8B3392103667E68C78F4448EA52E9E78B55A70401F48668FA988CDC0588E884F7B0713B58A90'
 });
 
-// client.recordEvent('purchases', {
-//   item: 'Avocado'
-// });
+client.recordEvent('purchases', {
+  item: 'Avocado'
+});
  
 
-const purchase = {
-  username: 'John Smith',
-  item: 'avocado',
-  price: 4.50
-};
+// const purchase = {
+//   username: 'John Smith',
+//   item: 'avocado',
+//   price: 4.50
+// };
 
-client
-  .recordEvent('purchases', purchase)
-  .then(result => {
-    console.log('Success', result);
-  })
-  .catch(err => {
-    console.log('Error', err);
-  });
+// client
+//   .recordEvent('purchases', purchase)
+//   .then(result => {
+//     console.log('Success', result);
+//   })
+//   .catch(err => {
+//     console.log('Error', err);
+//   });
